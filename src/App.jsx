07fdad7395f76d9db6811522c0db8a7940aced20
developer80555 <developer8055sdr@gmail.com>
2024-09-13 16:36:19 +0530

    Creating Job Portal

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./App.css";
+import AppLayout from "./layouts/app-layout";
+import LandingPage from "./pages/landing";
+import Onboarding from "./pages/onboardig";
+import JobListing from "./pages/job-listing";
+import JobPage from "./pages/job";
+import PostJob from "./pages/post-job";
+import SavedJobs from "./pages/save-job";
+import MyJobs from "./pages/my-jobs";
+import { ThemeProvider } from "./components/theeme-provider";
+import ProtectedRoute from "./components/protected-route";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: (
+          <ProtectedRoute>
+            <Onboarding />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/jobs",
+        element: (
+          <ProtectedRoute>
+            <JobListing />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/job/:id",
+        element: (
+          <ProtectedRoute>
+            <JobPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/post-job",
+        element: (
+          <ProtectedRoute>
+            <PostJob />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/saved-jobs",
+        element: (
+          <ProtectedRoute>
+            <SavedJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/my-jobs",
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/components/header.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Link, useSearchParams } from "react-router-dom";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { BriefcaseBusiness, Heart, PenBox } from "lucide-react";
+
+const Header = () => {
+  const [showSignIn, setShowSignIn] = useState(false);
+  const [search, setSearch] = useSearchParams();
+
+  useEffect(() => {
+    if (search.get("sign-in")) {
+      setShowSignIn(true);
+    }
+  }, [search]);
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setShowSignIn(false);
+      setSearch({});
+    }
+  };
+  return (
+    <>
+      <nav className="py-4 flex justify-between items-center">
+        <Link>
+          <img src="/logo.png" className="h-20" />
+        </Link>
+
+        <header>
+          {/* <Button variant="outline">Login</Button> */}
+          <div>
+            <SignedOut>
+              <Button variant="outline" onClick={() => setShowSignIn(true)}>
+                Login
+              </Button>
+            </SignedOut>
+            <SignedIn>
+              <div className="flex gap-5 items-center md:gap-10 lg:gap-20 ">
+                <Link to="/post-job">
+                  <Button variant="destructive" className="rounded-full">
+                    <PenBox size={20} className="mr-2" />
+                    Post a Job
+                  </Button>
+                </Link>
+
+                <UserButton
+                  appearance={{
+                    elements: {
+                      avatarBox: "w-10 h-10",
+                    },
+                  }}
+                >
+                  <UserButton.MenuItems>
+                    <UserButton.Link
+                      label="My Jobs"
+                      labelIcon={<BriefcaseBusiness size={15} />}
+                      href="/my-jobs"
+                    />
+                    <UserButton.Link
+                      label="Saved jobs"
+                      labelIcon={<Heart size={15} />}
+                      href="/saved-jobs"
+                    />
+                  </UserButton.MenuItems>
+                </UserButton>
+              </div>
+            </SignedIn>
+          </div>
+        </header>
+      </nav>
+      {showSignIn && (
+        <div
+          className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+          <SignIn
+            signUpForceRedirectUrl="/onboarding"
+            fallbackRedirectUrl="/onboarding"
+          />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Header;
